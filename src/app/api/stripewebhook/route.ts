badc58a99eff79a3a3d8b7b685a0bad5ee210c8a
Ed <ethereos@gmail.com>
2023-09-04 17:24:19 -0400

    fixed metadata ts error

--- src/app/api/stripewebhook/route.ts
+++ src/app/api/stripewebhook/route.ts
@@ -4,6 +4,7 @@ import { signups } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 import { headers } from "next/headers"
+import { any } from "prop-types";
 const stripe = new Stripe(process.env["STRIPE_SECRET_KEY"]!, {typescript: true, apiVersion: "2023-08-16"});
 
 export async function POST(request: Request) {
@@ -18,8 +19,8 @@ export async function POST(request: Request) {
     console.log(body)
     switch(event.type){
         case 'checkout.session.completed':
-            console.log("META PULL")
-            const checkmeta = event.data.object.metadata
+            const checkobj: {metadata?: any} = event.data.object
+            const checkmeta = checkobj.metadata
             console.log(checkmeta.signupid)
             await db.update(signups).set({ paystatus: 'paid' }).where(eq(signups.id, checkmeta.signupid));
             break;
